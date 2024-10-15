b7f400a41388c4ea84e863d3ed8f564373e3dba6
Nicol3w3 <125313964+Nicol3w3@users.noreply.github.com>
2024-10-14 22:50:43 -0300

    Main Menu Scene Added

---
+++ THDV-12f1/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GameIsPaused = false;
+    
+    public GameObject PauseMenuUI;
+
+    // [SerializeField]
+
+    // Update is called once per frame
+    void Start () {
+        PauseMenuUI.SetActive(false);
+    }
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        
+        }
+    }
+
+    public void Resume ()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause ()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log ("Quitting Game...");
+        Application.Quit();
+    }
+}
---
+++ THDV-12f1/Assets/Scripts/SO/PlayerDataSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObject/PlayerDataSO")]
+public class PlayerDataSO : ScriptableObject
+{
+    public int jumpSpeed;
+}
