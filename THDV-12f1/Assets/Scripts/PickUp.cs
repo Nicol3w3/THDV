6744c79ac765824dce8353608ed91047a9c848ab
Nicol3w3 <125313964+Nicol3w3@users.noreply.github.com>
2024-10-29 19:04:41 -0300

    PickUp and Pickupable Scripts Added

---
+++ THDV-12f1/Assets/Scripts/PickUp.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform holdPosition;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 50.0f, Color.red);
+        CheckForPickup();
+    }
+    
+    private void CheckForPickup()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 20.0f))
+        {
+            if(Input.GetKey(KeyCode.Mouse0))
+            {
+                if(hit.transform.gameObject.GetComponent<Pickupable>())
+                {
+                    //do something
+                    hit.transform.gameObject.GetComponent<Pickupable>().SetPickedState(holdPosition);
+                }
+            }
+        }
+
+        if(Input.GetKey(KeyCode.Mouse1))
+        {
+            if(hit.transform.GetComponent<Pickupable>())
+            {
+                hit.transform.gameObject.GetComponent<Pickupable>().SetDroppedState();
+            }
+        }
+    }
+}
